9632e0c5ac25f98f65bb9cf08c07c182942a5853
SEspy78 <baokhang212121@gmail.com>
2025-06-05 14:04:15 +0700

    Footer Header for home page

---
+++ ChildVaccination_System/src/Pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+
+
+
+const HomePage: React.FC = () => (
+  <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-200">
+    <Header />
+    <Footer/>
+  </div>
+);
+
+export default HomePage;
